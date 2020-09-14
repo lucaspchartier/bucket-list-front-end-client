81a985c2d500db33f85d50a40a9b8ab2b0b96865
Lucas Chartier <lucaspchartier@gmail.com>
2020-09-13 20:47:48 -0400

    Change production link to new deployed API on Heroku

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://morning-tor-51744.herokuapp.com',
+  production: 'https://cryptic-lake-24962.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
