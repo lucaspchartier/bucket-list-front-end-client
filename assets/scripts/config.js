072e7ab396cfa0628e3fb408bd1575f086c5e01c
Lucas Chartier <lucaspchartier@gmail.com>
2020-09-16 21:17:47 -0400

    Add cors-anywhere proxy to production url per article on Medium

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://cryptic-lake-24962.herokuapp.com',
+  production: 'https://cors-anywhere.herokuapp.com/https://cryptic-lake-24962.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
